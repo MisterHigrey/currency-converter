63971bcb2dbeb4796f6e06b08e37dce39eb27ee5
Gleb <gleb.programmer.ua@gmail.com>
2022-12-28 19:40:17 +0200

    app

---
+++ js/state.js
@@ -0,0 +1,19 @@
+export default {
+  url: "https://v6.exchangerate-api.com/v6/23b1d2b0fb7e6daae9ea525a",
+  codes: [],
+  pair: {
+    from: "",
+    to: "",
+  },
+  amount: "",
+  loading: false,
+  currentTab: "convert",
+  currency: {
+    code: "USD",
+  },
+  currencies: ["USD", "EUR"],
+  actions: {
+    remove: "remove",
+    change: "change",
+  },
+};
